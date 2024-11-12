eb6f77cc67a38596c2e21a67f72c1de9297355e5
Marvin <ymarvintan@gmail.com>
2024-11-12 23:10:46 +0800

    update

--- Chapter_Twelve/blue_bird_game.py
+++ Chapter_Twelve/blue_bird_game.py
@@ -2,25 +2,23 @@ import sys
 import pygame
 from ship import Ship
 from bird import Bird
-from settings import Settings
-class AlienInvasion:
+class BlueBird:
     """Overall class to manage game assets and behaviour"""
     def __init__(self):
         """initialize the game and create game resources"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.settings = Settings()
+
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-        self.ship = Ship(self)
+        self.bird = Bird(self)
         #Set background color
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
-            self.ship.update()
             self._update_screen()
             self.clock.tick(60) #frame rate of 60fps
 
@@ -29,36 +27,16 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                self._check_keydown_events(event)
-            elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event)
     
-    def _check_keydown_events(self,event):
-        if event.key == pygame.K_RIGHT:
-            #Move the ship to the right
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            #Move the ship to the left
-            self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit()
-
-    def _check_keyup_events(self,event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
-
     def _update_screen(self):
         """Update image on screen, and flip to the new screen."""
         self.screen.fill(self.bg_color)
         #draw the ship onto the screen
-        self.ship.blitme()
+        self.bird.blitme()
         #Make the most recently drawn screen visible
         pygame.display.flip()
 
 if __name__ == '__main__':
     #make a game instance, and run the game
-    ai = AlienInvasion()
+    ai = BlueBird()
     ai.run_game()
---
+++ Chapter_Twelve/settings.py
@@ -0,0 +1,5 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+    def __init__(self):
+        #Ship settings
+        self.ship_speed = 12
